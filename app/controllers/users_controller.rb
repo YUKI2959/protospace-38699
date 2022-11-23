fcae69779b0a420d205946164266d316251c26cc
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-11-23 23:25:25 +0900

    ユーザー管理機能+ログイン状態の制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @name = user.name
-    @profile = user.profile
-    @occupation = user.occupation
-    @position = user.position
+    @user = User.find(params[:id])
+    @prototypes = @user.prototypes
   end
 end
